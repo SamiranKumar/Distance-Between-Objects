8c179d39931e0a05fd75932a8b80afdfc5b68a53
Samiran <samirankumar11@gmail.com>
2017-05-07 11:45:01 +0600

    first commit

---
+++ Distance Between Objects (Vector3)/Assets/Distance.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Distance : MonoBehaviour {
+
+
+	public GameObject Cube1;
+	public GameObject Cube2;
+
+	public float mDistances;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		mDistances = Vector3.Distance(Cube1.transform.position,Cube2.transform.position);
+		if (mDistances < 3) {
+		
+			Debug.Log ("Enamy:" + mDistances);		
+		}
+	}
+}
